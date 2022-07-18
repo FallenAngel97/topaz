50ccc0abda97759a5097dd1631372affdab1084b
Oleksii Demennikov <odemennikov@lohika.com>
2022-07-18 19:13:03 +0300

    Added the description

--- index.py
+++ index.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 
 import argparse
-import json
 
-from git import Repo
+from actions.init_topaz_action import InitTopazAction
+from actions.restore_topaz_action import RestoreAction
 
-def restore():
-    print("🪖 Restoring the policies...")
-    topaz_json = open('topaz.json')
-    json_contents = json.load(topaz_json)
-    for git_repo in json_contents['git']:
-        print("Cloning " + git_repo + "...")
-        Repo.clone_from(git_repo, "./_topaz")
+topaz_description = '''
+💎 Topaz is the CLI utility for the Open Policy Agent. 
+
+Thegeneral idea is to introduce the package manager, which can perform basic tasks, such as managing the dependencies
+in a more convenient way
+'''
+
+opa_link = '''
+More about OPA: https://openpolicyagent.org
+'''
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('restore', nargs='?')
-    args = parser.parse_args()
-    if args.restore != None:
-        restore()
+    parser = argparse.ArgumentParser(
+        description = topaz_description,
+        epilog = opa_link
+    )
+    parser.add_argument('restore', nargs='?', action=RestoreAction)
+    parser.add_argument('init', nargs='?', action=InitTopazAction)
+    args = vars(parser.parse_args())
+    if not any(args.values()):
+        parser.error('No arguments provided.')
 
 if __name__ == "__main__":
    main()
